cebefd4b095d60cb24a50bc5eb21e6998be35001
jws2170 <mpart2077@gmail.com>
2024-03-13 21:19:56 +0900

    [FIX] CarMovement

--- Assets/Script/CarMovement.cs
+++ Assets/Script/CarMovement.cs
@@ -5,44 +5,61 @@ using System;
 
 public class CarMovement : MonoBehaviour
 {
-    public enum Axel
+    public WheelCollider[] wheels = new WheelCollider[4];
+    public GameObject[] wheelMesh = new GameObject[4];
+    public float torque = 200;
+    public float steeringMax = 4;
+
+    private void Start()
     {
-        Frount,
-        Rear
+
     }
 
-    [Serializable]
-    public struct wheel
+    private void FixedUpdate()
     {
-        public GameObject wheelModel;
-        public WheelCollider wheelColl;
-        public Axel axel;
-    }
+        AnimateWheels();
 
-    public float maxAcce = 30f;
-    public float breakAcce = 50f;
-
-    public List<wheel> wheels;
-
-    public float moveInput;
-
-    private Rigidbody rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void GetInput()
-    {
-        moveInput = Input.GetAxis("Vertical");
-    }
-
-    void Move()
-    {
-        foreach(var wheel in wheels)
+        if (Input.GetKey(KeyCode.W))
         {
-            wheel.wheelColl.motorTorque = moveInput * 600 * maxAcce * Time.deltaTime;
+            for(int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = torque;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = 0;
+            }
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            for (int i = 0; i < wheels.Length - 2; i++)
+            {
+                wheels[i].steerAngle = Input.GetAxis("Horizontal") * steeringMax;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < wheels.Length - 2; i++)
+            {
+                wheels[i].steerAngle = 0;
+            }
+        }
+
+        void AnimateWheels()
+        {
+            Vector3 wheelPos = Vector3.zero;
+            Quaternion wheelRot = Quaternion.identity;
+
+            for(int i = 0; i < 4; i++)
+            {
+                wheels[i].GetWorldPose(out wheelPos, out wheelRot);
+                wheelMesh[i].transform.position = wheelPos;
+                wheelMesh[i].transform.rotation = wheelRot;
+            }
         }
     }
 }
