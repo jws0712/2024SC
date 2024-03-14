7c5775f7c06ce2b9d89e5e9f6d2f0e97a78cb6a9
jws2170 <mpart2077@gmail.com>
2024-03-14 23:38:18 +0900

    [ADD] Track

--- Assets/Script/CarMovement.cs
+++ Assets/Script/CarMovement.cs
@@ -5,76 +5,32 @@ using System;
 
 public class CarMovement : MonoBehaviour
 {
-
-    private float vertical;
-    private float horizontal;
-    private bool handbrake;
     public WheelCollider[] wheels = new WheelCollider[4];
     public GameObject[] wheelMesh = new GameObject[4];
     public float motortorque = 200;
     public float steeringMax = 4;
-    public float breakeTorque = 1000;
-    Rigidbody rb;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         //Cursor.lockState = CursorLockMode.Locked;
         //Cursor.visible = false;
     }
 
     private void FixedUpdate()
     {
-        input();
         AnimateWheels();
-        moveVehivle();
-        steerVehicle();
-        breakVehicle();
     }
 
     void moveVehivle()
     {
-        for (int i = 0; i < wheels.Length; i++)
-        {
-            wheels[i].motorTorque = vertical * motortorque;
-        }
-        
 
     }
 
     void steerVehicle()
     {
-        for (int i = 0; i < wheels.Length - 2; i++)
-        {
-            wheels[i].steerAngle = horizontal * steeringMax;
-        }
-    }
-
-    void breakVehicle()
-    {
-        if (handbrake)
-        {
-            for (int i = 2; i < wheels.Length; i++)
-            {
-                wheels[i].brakeTorque = breakeTorque;
-            }
-        }
-        else
-        {
-            for (int i = 2; i < wheels.Length; i++)
-            {
-                wheels[i].brakeTorque = 0;
-            }
-        }
 
     }
 
-    void input()
-    {
-        vertical = Input.GetAxis("Vertical");
-        horizontal = Input.GetAxis("Horizontal");
-        handbrake = (Input.GetAxis("Jump") != 0) ? true : false;
-    }
     void AnimateWheels()
     {
         Vector3 wheelPos = Vector3.zero;
